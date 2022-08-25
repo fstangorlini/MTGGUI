79b87704c93d9d25faeb1c8046d2cd2b301f3b0a
Felipe <32676718+fstangorlini@users.noreply.github.com>
2022-08-25 16:34:28 -0300

    Update MTGGUI.py

--- MTGGUI.py
+++ MTGGUI.py
@@ -17,19 +17,16 @@ from tkinter import *
 import tkinter as tk
 import queue
 import threading
-from dataclasses import dataclass
-import json
-import os
 from tkinter.font import BOLD
-import requests
-import zipfile
-import random
-import numpy as np
 from PIL import Image, ImageTk
-from datetime import datetime
+from mtgjson import Mtgjson
+from card import Card
 
 ######################################################################################################
 # Classes
+
+# Threaded task to handle image download and
+# booster generation without freezing up UI
 class ThreadedTask(threading.Thread):
 
     def __init__(self, queue_:queue, set_name:str):
@@ -38,217 +35,10 @@ class ThreadedTask(threading.Thread):
         self.set_name = set_name
         
     def run(self):
-        #Worker(self.queue_,self.fileX,self.fileY)
         m = Mtgjson(self.queue_)
         booster = m.generate_booster(self.set_name)
-        self.queue_.put((2,booster))
-
-@dataclass
-class Card:
-
-    name:str
-    type:str
-    set:str
-    rarity:str
-    uuid:str
-    image_url:str
-    rarity_converter = {}
-    cache_dir_img:str
-    image_resolution:str
-    queue_:queue
-    image:Image
-
-    def __init__(self,json_data:json, queue_:queue):
-        #print(f'Initializing Card:\n{json_data}')
-        self.image_resolution = 'normal' #small, normal, large, png, art_crop, or border_crop. Defaults to large.
-        self.rarity_converter = {'mythic':'M','rare':'R','uncommon':'U','common':'C'}
-        self.cache_dir_img = './cache/img/'
-        self.file_extension = '.png'
-        self.queue_ = queue_
-        #print(f'Initializing card {name}')
-        if json_data is not None:
-            self.name = json_data['name']
-            self.set = json_data['setCode']
-            self.uuid = json_data['identifiers']['scryfallId']
-            self.image_url = 'https://api.scryfall.com/cards/'+self.uuid+'?format=image&face=front&version='+self.image_resolution
-            self.rarity = json_data['rarity']
-            self.type = json_data['type']
-        if not os.path.isdir(self.cache_dir_img): os.makedirs(self.cache_dir_img)
-
-    def __get_image__(self):
-        if(os.path.isfile(self.cache_dir_img+self.uuid+self.file_extension)):
-            img = Image.open(self.cache_dir_img+self.uuid+self.file_extension)
-            self.image = img
-            return img
-        else:
-            print(f'Image [{self.name}] not in cache. Fetching from {self.image_url}',end='. ')
-            self.queue_.put((0,'Image ['+self.name+'] not in cache. Fetching from '+self.image_url))
-            try:
-                img = Image.open(requests.get(self.image_url, stream=True).raw)
-                img.save(self.cache_dir_img+self.uuid+self.file_extension)
-                print('OK!')
-                self.queue_.put((0,'OK!'))
-            except:
-                print('NOK')
-                self.queue_.put((0,'Error downloading...'))
-                img = Image.open('./res/mtg-card-back.png')
-            self.image = img
-            return img
-
-    def __str__(self):
-        ret = '-----------------------------------'
-        ret = ret + '\n[Name]\t\t' + self.name
-        ret = ret + '\n[Set]\t\t' + self.set
-        ret = ret + '\n[Rarity]\t' + self.rarity
-        ret = ret + '\n[Url]\t\t' + self.image_url
-        ret+= '\n-----------------------------------\n'
-        return ret
-
-    def __repr__(self):
-        return self.name
-
-@dataclass
-class Mtgjson:
-
-    cache_dir:str
-    url:str
-    json_data:json
-    sets:list[str]
-    queue_:queue
-
-    def __init__(self, queue_:queue):
-        self.cache_dir = './cache/metadata/'
-        url_base_pre = 'https://mtgjson.com/api/v5/'
-        json_file = 'AllSetFiles.zip'
-        self.file_extension = '.json'
-        self.url_composed = url_base_pre+json_file
-        self.queue_ = queue_
-        if not os.path.isdir(self.cache_dir):os.makedirs(self.cache_dir)
-        if not os.path.isfile(self.cache_dir+json_file):
-            self.queue_.put((0,'Database not found in cache. Downloading...'))
-            # Download
-            req = requests.get(self.url_composed)
-            # save
-            with open(self.cache_dir+json_file,'wb') as output_file:
-                output_file.write(req.content)
-            # Unpack
-            self.queue_.put((0,'Unpacking database...'))
-            with zipfile.ZipFile(self.cache_dir+json_file, 'r') as zip_ref:
-                zip_ref.extractall(self.cache_dir)
-        self.sets = [f[:-len(self.file_extension)] for f in os.listdir(self.cache_dir) if os.path.isfile(os.path.join(self.cache_dir, f)) and f.endswith(self.file_extension)]
-
-    def generate_booster(self, set_name:str):
-        self.queue_.put((0,'Generating a new booster from set ['+set_name+']'))
-        if set_name not in self.sets:
-            #print(f'Set [{set_name}] could not be found.')
-            return None
-        with open(self.cache_dir+set_name+self.file_extension, encoding='UTF-8') as f: json_data = json.load(f)
-        card_distribution = {'rare':1,'uncommon':3,'common':10,'Basic Land':1} #15 total
-        cards_in_booster = []
-        rares = self.__get_cards_by_rarity__('rare',json_data)
-        uncommons = self.__get_cards_by_rarity__('uncommon',json_data)
-        commons = self.__get_cards_by_rarity__('common',json_data)
-        basic_lands = self.__get_cards_by_rarity__('Basic Land', json_data)
-        rc={'rare':rares,'uncommon':uncommons,'common':commons,'Basic Land':basic_lands}
-        for rarity, quantity in card_distribution.items():
-            cards_in_booster.extend(random.sample(self.__get_cards_by_rarity__(rarity,json_data), k=quantity))
-        #self.display(cards_in_booster)
-        self.__fetch_images__(cards_in_booster)
-        self.queue_.put((1,'Booster generated successfully'))
-        return cards_in_booster
-    
-    def __get_card_by_name__(self, card_name:str, json_data:json):
-        '''
-        #Non pythonic way
-        cards = json_data['data']['cards']
-        for card in cards:
-            if card['name']==card_name: return card
-        return []
-        '''
-        # Pythonic way
-        return list(filter(lambda card: card['name'] == card_name, json_data['data']['cards']))
-    
-    def __get_cards_by_rarity__(self, rarity:str, json_data:json):
-        card_list = []
-        #TODO implement mythic
-        if rarity in ['rare','uncommon','common']: 
-            for json_card in list(filter(lambda card: ((card['rarity'] == rarity) and ('Basic Land' not in card['type'])), json_data['data']['cards'])):
-                card_list.append(Card(json_card, self.queue_))
-        if rarity in ['Basic Land']:
-            for json_card in list(filter(lambda card: ('Basic Land' in card['type']), json_data['data']['cards'])):
-                card_list.append(Card(json_card, self.queue_))
-        return card_list
-    
-    def __get_generated_booster_images__(self):
-        return [self.cache_dir+f for f in os.listdir(self.cache_dir) if f.startswith('booster') and f.endswith(self.file_extension)]
-
-    def __delete_oldest_image__(self):
-        oldest_file = min(self.__get_generated_booster_images__(), key=os.path.getctime)
-        os.remove(oldest_file)
-
-    def __control_cache__(self):
-        while len(self.__get_generated_booster_images__()) > 15:
-            print(f'Cache size too big. Deleting a few previously generated booster images to save some disk space...')
-            self.__delete_oldest_image__()
-
-    def display(self, booster_cards:list[Card]):
-        self.queue_.put((0,'Displaying booster...'))
-        if(len(booster_cards)!=15): return False
-        img = self.__assemble__(self.__fetch_images__(booster_cards))
-        str_date_time = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
-        filename = booster_cards[0].cache_dir_img+'booster-'+str_date_time+'.png'
-        img.save(filename) #PIL
-        #img.show() #PIL
-        self.__control_cache__() # keeps only a few last generated booster packs not to consume too much space
-
-    def __overlay_image__(self, l_img, s_img, x_offset, y_offset):
-        #print(f'overlay_image')
-        #print(f'l_img=({l_img.shape[0]},{l_img.shape[1]})')
-        #print(f's_img=({s_img.shape[0]},{s_img.shape[1]})')
-        #height = s_img.shape[0] #cv2
-        #width = s_img.shape[1] #cv2
-        height = s_img.height #PIL
-        width = s_img.width #PIL
-        l_img[y_offset:y_offset+height, x_offset:x_offset+width] = s_img
-        return l_img
-
-    def __assemble__(self, card_image_list_as_image:list):
-        x_spacing = 50
-        y_spacing = 50
-        rows = [0,1,2]
-        cols = [0,1,2,3,4]
-        x_max = 0
-        y_max = 0
-        for card_image in card_image_list_as_image:
-            #height = card_image.shape[0] #cv2
-            #width = card_image.shape[1] #cv2
-            height = card_image.height #PIL
-            width = card_image.width #PIL
-            if x_max < height: x_max = height
-            if y_max < width: y_max = width
-        x_full_length = (x_max*len(rows))+(len(rows)*x_spacing)+x_spacing
-        y_full_length = (y_max*len(cols))+(len(cols)*y_spacing)+y_spacing
-        img = np.zeros([x_full_length,y_full_length,3],dtype=np.uint8)
-        #img.fill(255) #make image white
-        i=0
-        for col in cols:
-            for row in rows:
-                card_image = card_image_list_as_image[i]
-                height = card_image.height #PIL
-                width = card_image.width #PIL
-                x = x_spacing+(x_spacing*row)+(row*height)
-                y = y_spacing+(y_spacing*col)+(col*width)
-                #print(f'(r{row},c{col} -> overlay at ({x},{y})')
-                img = self.__overlay_image__(img,card_image_list_as_image[i],y,x)
-                i+=1
-        img = Image.fromarray(img) #PIL
-        return img
-    
-    def __fetch_images__(self, booster_cards:list[Card]):
-        card_image_list_as_image = []
-        for card in booster_cards:
-            card_image_list_as_image.append(card.__get_image__())
-        return card_image_list_as_image
+        if booster is not None: self.queue_.put((2,booster))
+        else: self.queue_.put((1,'Error generating booster. Ensure the set code is valid and try again.'))
 
 ######################################################################################################
 # GUI
@@ -257,18 +47,27 @@ class MTGGUI(tk.Frame):
     queue_:queue
     image_list:list
     
+    # generate button action
     def __action_button_generate__(self):
-        self.__disable_buttons__()
-        self.progress_bar.start()
-        ThreadedTask(self.queue_,self.stringvar_sets.get()).start()
-        return
+        selected_set = self.stringvar_sets.get()
+        if selected_set in self.SETS:
+            self.images.clear()
+            self.display_index = 1
+            self.__update_images__()
+            self.__disable_buttons__()
+            self.progress_bar.start()
+            ThreadedTask(self.queue_,selected_set).start()
+        else:
+            self.__put_text_in_status__('Error generating booster. Ensure the set code is valid and try again.')
     
+    # next button action
     def __action_button_next__(self):
         if self.display_index >= 15: self.display_index = 0
         self.display_index+=1
         self.__update_images__()
         return
     
+    # previous button action
     def __action_button_prev__(self):
         if self.display_index <= 1: self.display_index = 16
         self.display_index-=1
@@ -285,6 +84,7 @@ class MTGGUI(tk.Frame):
         self.button_next.config(state='normal')
         self.button_prev.config(state='normal')
     
+    # populates list of all images once. self.images is a list of images with tuple(small,large)
     def __populate_image_list__(self, booster:list[Card]):
         self.images.append((self.img_card_back[0],self.img_card_back[1]))
         for card in booster:
@@ -293,11 +93,13 @@ class MTGGUI(tk.Frame):
             self.images.append((s,l))
         self.images.append((self.img_card_back[0],self.img_card_back[1]))
     
+    # updates an image into UI Label
     def __update_image__(self, label:Label, image:Image):
         img = ImageTk.PhotoImage(image)
         label.configure(image = img)
         label.image = img
 
+    # updates all images into UI labels (left, center, right)
     def __update_images__(self):
         if(len(self.images)!=17):
             self.__update_image__(self.label_img_mid, self.img_card_back[1])
@@ -308,25 +110,35 @@ class MTGGUI(tk.Frame):
             self.__update_image__(self.label_img_left, self.images[self.display_index-1][0])
             self.__update_image__(self.label_img_right, self.images[self.display_index+1][0])
 
+    # puts text into status text field at the bottom
+    def __put_text_in_status__(self, text:str):
+        self.textfield_status.config(state='normal')
+        self.textfield_status.delete(0,END)
+        self.textfield_status.insert(0,text)
+        self.textfield_status.config(state='disabled')
+
+    # process messages on queue (shared with other classes)
     def __process_queue__(self):
         msg = None
         if(self.queue_.empty()==False):
             msg = self.queue_.get(0)
+            #msg[0]==1 only when info/error msg is passed
             if(msg[0]==1):
                 self.progress_bar.stop()
                 self.__enable_buttons__()
+            #msg[0]==2 only when booster is generated successfully
             if(msg[0]==2):
                 self.booster = msg[1]
                 self.__populate_image_list__(self.booster)
                 self.__update_images__()
         return msg
     
+    # runs every 100ms to update UI elements
     def __update_root__(self):
         self.root.after(100, self.__update_root__)
         if(self.queue_.empty()==False):
             msg = self.__process_queue__()
-            self.textfield_status.delete(0,END)
-            self.textfield_status.insert(0,msg[1])
+            if(msg[0]!=2): self.__put_text_in_status__(msg[1])
     
     def __custom_init__(self):
         #######################################################################
@@ -337,10 +149,10 @@ class MTGGUI(tk.Frame):
         
         #######################################################################
         # Create elements        
-        self.label_subtitle         = Label(self.root,text=self.SUBTITLE,font=("Segoe UI", 16, BOLD))
-        self.textfield_status       = Entry(self.root,width=105,textvariable=self.stringvar_text_status)
         self.stringvar_text_status  = StringVar()
         self.stringvar_sets         = StringVar()
+        self.label_subtitle         = Label(self.root,text=self.SUBTITLE,font=("Segoe UI", 16, BOLD))
+        self.textfield_status       = Entry(self.root,width=105,textvariable=self.stringvar_text_status)
         self.progress_label         = Label(self.root,text=self.label_progress,font=("Segoe UI", 10))
         self.progress_bar           = ttk.Progressbar(orient="horizontal",length=self.width, mode="determinate")
         self.label_sets             = Label(self.root,text='Set',width=7,font=("Segoe UI", 10))
@@ -370,10 +182,8 @@ class MTGGUI(tk.Frame):
     def __post_init__(self):
         #######################################################################
         # Post
-        self.textfield_status.delete(0,END)
-        self.textfield_status.insert(0,'Please inform Set code and click Generate button. (I.E. LEA = Legacy Alpha)')
+        self.__put_text_in_status__('Please inform Set code and click Generate button. (I.E. LEA = Legacy Alpha)')
         self.entry_sets.insert(0,'LEA')
-        self.textfield_status.config(state='disabled')
         self.button_next.config(state='disabled')
         self.button_prev.config(state='disabled')
         self.__update_images__()
@@ -463,4 +273,5 @@ class MTGGUI(tk.Frame):
         self.__post_init__()
        
 # Initialize
-g = MTGGUI(queue.Queue())
+if __name__ == "__main__":
+    g = MTGGUI(queue.Queue())
